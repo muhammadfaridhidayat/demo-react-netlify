573a982a89687c5eb817951f60dc37db1a342964
muhammadfaridhidayat <muhammadfaridhidayat.as4@gmail.com>
2024-10-23 00:14:17 +0800

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Muhammad Farid Hidayat" // TODO: replace this
+const studentId = "FS10514679" // TODO: replace this
+
+export { url, studentName, studentId }
